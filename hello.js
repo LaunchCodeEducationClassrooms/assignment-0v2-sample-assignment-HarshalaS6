1146efc1d192c9297a48748947982b30fa8e18c4
Harshala Sadhale <harshala2018@gmail.com>
2021-02-07 22:31:39 +0000

    Syntax error is removed

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
 	return "Hello, World!";
-};
+}
  
 module.exports = hello; 
 
